77de924b524e1f68feeddbada0092f8b17003c3a
Dmitry Logvinovich <dlogvinovich@yahoo.com>
2018-01-14 22:07:42 +0100

    Folder tree cell renderer added

---
+++ src/main/java/ch/mardmi/renamingutility/model/FolderTreeCellRenderer.java
@@ -0,0 +1,54 @@
+package ch.mardmi.renamingutility.model;
+
+import java.awt.Component;
+import java.io.File;
+
+import javax.swing.JLabel;
+import javax.swing.JTree;
+import javax.swing.filechooser.FileSystemView;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+/**
+ * Renderer für TreeNode vom JTree.
+ */
+public class FolderTreeCellRenderer extends DefaultTreeCellRenderer {
+
+	private static final long serialVersionUID = 1L;
+
+	private FileSystemView fileSystemView;
+
+    private JLabel label;
+
+    public FolderTreeCellRenderer() {
+        label = new JLabel();
+        label.setOpaque(true);
+        fileSystemView = FileSystemView.getFileSystemView();
+    }
+
+    @Override
+    public Component getTreeCellRendererComponent(
+        JTree tree,
+        Object value,
+        boolean selected,
+        boolean expanded,
+        boolean leaf,
+        int row,
+        boolean hasFocus) {
+
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode)value;
+        File file = (File)node.getUserObject();
+        label.setIcon(fileSystemView.getSystemIcon(file));
+        label.setText(fileSystemView.getSystemDisplayName(file));
+        label.setToolTipText(file.getPath());
+
+        if (selected) {
+            label.setBackground(backgroundSelectionColor);
+            label.setForeground(textSelectionColor);
+        } else {
+            label.setBackground(backgroundNonSelectionColor);
+            label.setForeground(textNonSelectionColor);
+        }
+        return label;
+    }
+}
