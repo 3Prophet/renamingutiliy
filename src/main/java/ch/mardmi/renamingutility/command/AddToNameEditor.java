be94aa9bb14ffa03c0578782056f94645baab8b6
Dmitry Logvinovich <dlogvinovich@yahoo.com>
2018-01-28 19:05:38 +0100

    Documentation for FileNameEditor Interface and AddToNameEditor

--- src/main/java/ch/mardmi/renamingutility/command/AddToNameEditor.java
+++ src/main/java/ch/mardmi/renamingutility/command/AddToNameEditor.java
@@ -1,14 +1,41 @@
 package ch.mardmi.renamingutility.command;
 
+/**
+ * Implementiert einer oder mehrerer der folgenden Funktionen:
+ * Addierung von Prefix, Suffix, Einführung von Symbolen im Dateiname.
+ * 
+ * 
+ * @author Dmitry Logvinovich
+ *
+ */
 public class AddToNameEditor implements FileNameEditor {
 	
+	/**
+	 * Prefix für den Dateiname
+	 */
 	private String prefix;
+	
+	/**
+	 * Suffix für den Dateiname
+	 */
 	private String suffix;
+	
+	/**
+	 * Symbole, die im Dateiname eingefügt werden sollen
+	 */
 	private String insert;
+	
+	/**
+	 * Stelle im Dateiname wo die Symbole eingefügt werden sollen
+	 */
 	private int position;
+	
+	/**
+	 * Definiert, ob Objekt der Klasse die Berechtigung für Datei Unbenennung hat
+	 */
 	private boolean selected;
-	private String fileName;
-
+	
+	
 	public AddToNameEditor( 
 							boolean selected,
 							String prefix,
@@ -26,10 +53,16 @@ public class AddToNameEditor implements FileNameEditor {
 		this.position = position;
 	}
 	
-	public AddToNameEditor(boolean selected) {
+	private AddToNameEditor(boolean selected) {
 		this.selected = selected;
 	}
-
+	
+	/**
+	 * Unbenennt Datei when die entsprechende Berechtigung erteilt wird
+	 * 
+	 * @param fileName Dateiname für Aenderung
+	 * @returns Neuen Dateiname
+	 */
 	@Override
 	public String editName(String fileName) {
 		
--- src/main/java/ch/mardmi/renamingutility/command/FileNameEditor.java
+++ src/main/java/ch/mardmi/renamingutility/command/FileNameEditor.java
@@ -1,7 +1,19 @@
 package ch.mardmi.renamingutility.command;
 
+/**
+ *  Schnittstelle für die Klasse die kapselt, wie Datei unbenennen werden soll
+ *  Jede bestimmte Klasse ändert nur Dateiname (soll Ohne Pfad und Endung eingegeben werden). 
+ *  
+ * @author Dmitry Logvinovich
+ *
+ */
 public interface FileNameEditor {
-	
+	/**
+	 * Unbenennt Datei
+	 * 
+	 * @param fileName Dateiname ohne Pfad und Endung
+	 * @return neuer Dateiname
+	 */
 	public String editName(String fileName);
 
 }
