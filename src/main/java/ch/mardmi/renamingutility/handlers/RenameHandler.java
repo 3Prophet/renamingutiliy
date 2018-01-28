14a1b61226abc3d844af62a0ca114d7586908ff4
Dmitry Logvinovich <dlogvinovich@yahoo.com>
2018-01-28 23:19:43 +0100

    RenameHandler first Implementation

--- src/main/java/ch/mardmi/renamingutility/handlers/RenameHandler.java
+++ src/main/java/ch/mardmi/renamingutility/handlers/RenameHandler.java
@@ -3,7 +3,7 @@ package ch.mardmi.renamingutility.handlers;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ClearActionHandler extends AbstractHandler implements ActionListener {
+public class RenameHandler extends AbstractHandler implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -11,9 +11,11 @@ public class ClearActionHandler extends AbstractHandler implements ActionListene
 				new Runnable() {
 					@Override
 					public void run() {
+						gui.getDirectoryModel().rename();
 						gui.resetPanels();
-						HandlerHelper.getHelper().execute(gui);
 					}
 				}).start();
+
 	}
+
 }
