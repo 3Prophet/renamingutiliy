dd6c7b1e038b5b05d253e17ad03870e58821f1a6
dlogvinovich <dmitry.logvinovich@scs.ch>
2018-01-30 14:20:38 +0100

    Dokumenting AbstractHandler

--- src/main/java/ch/mardmi/renamingutility/handlers/AbstractHandler.java
+++ src/main/java/ch/mardmi/renamingutility/handlers/AbstractHandler.java
@@ -2,10 +2,24 @@ package ch.mardmi.renamingutility.handlers;
 
 import ch.mardmi.renamingutility.view.MainFrame;
 
+/**
+ * Haltet Refernz zu MainFrame component. Auf diese Weise werden 
+ * Attributen von MainFrame für die Objekten der Klassen die von
+ * dieser Klasse vererben zugreifbar.
+ * 
+ * @author Dmitry Logvinovich
+ *
+ */
 public class AbstractHandler {
 	
+	/**
+	 * Referenz zu MainFrame
+	 */
 	protected static MainFrame gui;
 	
+	/**
+	 * @param gui GBO die für alle Handlers zugreifbar werden soll.
+	 */
 	public static void setGUI(MainFrame gui) {
 		AbstractHandler.gui = gui;
 	}
