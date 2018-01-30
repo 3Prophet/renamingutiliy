608bc9a61c553595f121a956418f0611dd69e6a4
dlogvinovich <dmitry.logvinovich@scs.ch>
2018-01-30 14:42:49 +0100

    Documentation for StatusModelListener interface

--- src/main/java/ch/mardmi/renamingutility/handlers/StatusModelListener.java
+++ src/main/java/ch/mardmi/renamingutility/handlers/StatusModelListener.java
@@ -2,6 +2,16 @@ package ch.mardmi.renamingutility.handlers;
 
 import ch.mardmi.renamingutility.model.StatusModel;
 
+/**
+ * Schnittstelle für die Status Modelle Listener.
+ * 
+ * @author Dlogvinovich Dmitry
+ *
+ */
 public interface StatusModelListener {
+	/**
+	 * Implementiert Aktion nach der StatusModel Veränderung.
+	 * @param statusModel
+	 */
 	public void statusModelChanged(StatusModel statusModel);
 }
