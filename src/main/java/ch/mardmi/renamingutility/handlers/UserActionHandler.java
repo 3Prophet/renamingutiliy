6ed6eadbb36bc727c2faaa48c4272b620d46eb5a
dlogvinovich <dmitry.logvinovich@scs.ch>
2018-01-30 14:30:01 +0100

    Documenting UserActionHandler

--- src/main/java/ch/mardmi/renamingutility/handlers/UserActionHandler.java
+++ src/main/java/ch/mardmi/renamingutility/handlers/UserActionHandler.java
@@ -5,16 +5,20 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.JCheckBox;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 
+/**
+ * Wird aufgerufen, wenn  Benutzer mit den Komponenten von Editor Platte interagiert.
+ * 
+ * @author Dmitry Logvinovich
+ *
+ */
 public class UserActionHandler extends AbstractHandler implements KeyListener, ChangeListener, ActionListener {
-
-
-
+	
+	/**
+	 * Wird aufgerufen when Benutzer mit Spinner von Editor Platten interagiert.
+	 */
 	@Override
 	public void stateChanged(ChangeEvent e) {
 		new Thread(
@@ -32,7 +36,10 @@ public class UserActionHandler extends AbstractHandler implements KeyListener, C
 
 	@Override
 	public void keyPressed(KeyEvent e) {}
-
+	
+	/**
+	 * Wird aufgerufen wenn Benutzer etwas in Text Felder eintippt.
+	 */
 	@Override
 	public void keyReleased(KeyEvent e) {
 		new Thread(
@@ -42,22 +49,21 @@ public class UserActionHandler extends AbstractHandler implements KeyListener, C
 						HandlerHelper.getHelper().execute(gui);
 						
 					}
-					
 				}).start();
-		
 	}
-
+	
+	/**
+	 * Wird aufgerufen when Check Boxes von Editor Platten selktiert oder deselektiert werden.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		new Thread(
 				new Runnable() {
-
 					@Override
 					public void run() {
 						HandlerHelper.getHelper().execute(gui);
 						
-					}
-					
+					}					
 				}).start();
 		
 	}
