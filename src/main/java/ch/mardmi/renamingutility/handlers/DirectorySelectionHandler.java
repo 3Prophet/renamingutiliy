581fe5f0b7f572c2a78a60deb4f51f9ebdaabd93
dlogvinovich <dmitry.logvinovich@scs.ch>
2018-01-30 15:46:05 +0100

    Documentation for DirectorySelectionHandler

--- src/main/java/ch/mardmi/renamingutility/handlers/DirectorySelectionHandler.java
+++ src/main/java/ch/mardmi/renamingutility/handlers/DirectorySelectionHandler.java
@@ -6,13 +6,27 @@ import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 
+/**
+ * Wird aufgeruft, wenn ein Verzeichniss in JTree selektiert wird. 
+ * Macht Ladung von Verzeichniss Ihnalt und aktualisiert {@link ch.mardmi.renamingutility.model.DirectoryContentModel},
+ * so dass die entsprechende JTable Inhalt vom selektierten Verzeichniss anzeigt.
+ * 
+ * @author Dmitry Logvinovich
+ *
+ */
 public class DirectorySelectionHandler extends AbstractHandler implements TreeSelectionListener {
 
 	/**
-	 * ausgewähltes Verzeichniss
+	 * Pfad zu dem selektierten Verzeichnis.
 	 */
 	private File selectedDir;
-
+	
+	/**
+	 * Aufgeruft, wenn ein Verzeichniss in JTree Selektiert wird. 
+	 * Macht Ladung von Verzeichniss Ihnalt und aktualisiert {@link ch.mardmi.renamingutility.model.DirectoryContentModel},
+	 * so dass die entsprechende JTable Inhalt vom selektierten Verzeichniss anzeigt.
+	 * 
+	 */
 	@Override
 	public void valueChanged(TreeSelectionEvent e) {
 		DefaultMutableTreeNode node = (DefaultMutableTreeNode) e.getPath().getLastPathComponent();
@@ -20,7 +34,12 @@ public class DirectorySelectionHandler extends AbstractHandler implements TreeSe
 		lazyLoadSelectedDirectory(node);
 		gui.getDirectoryModel().displayDir(selectedDir);
 	}
-
+	
+	/**
+	 * Lazy Loadung von Verzeichniss Inhalt. 
+	 * 
+	 * @param node Eine selektierte Knote
+	 */
 	private void lazyLoadSelectedDirectory(DefaultMutableTreeNode node) {
 		File[] files = gui.getFileSystemView().getFiles(selectedDir, true);
 		for (File file : files) {
